dce7373435b9580c9f610b33b8adbffa987aa031
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-03 14:32:45 -0800

    #2 animated drop down hover

--- src/components/global/_Header/Navigation.js
+++ src/components/global/_Header/Navigation.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Section from '../../entity/Section'
 import NavFooter from './NavFooter';
 import { Link } from 'react-router-dom';
+import { TriangleIcon } from '../../icons'
 
 export default function Navigation(props) {
+  const [toggleSubMenu, toggleSub] = useState(() => {return false})
+
+  const setHoverOn = () => {
+    toggleSub(true)
+  }
+
+  const setHoverOff = () => {
+    toggleSub(false)
+  }
+
+  const setClick = () => {
+    toggleSub(toggleSubMenu => !toggleSubMenu)
+  }
+
   const toggleMenu = props.toggle;
   const baseDelay = 0.075;
   return (
@@ -11,9 +26,13 @@ export default function Navigation(props) {
       <Section>
         <nav className="main">
           <ul>
-            <li className="has-submenu" style={{transitionDelay: `${baseDelay * 1}s`}}>
-              <button>FILM</button>
-              <ul>
+            <li 
+              className={ toggleSubMenu ? "has-submenu active" : "has-submenu"} style={{transitionDelay: `${baseDelay * 1}s`}}
+              onMouseEnter={setHoverOn}
+              onMouseLeave={setHoverOff}
+            >
+              <button onClick={setClick}>FILM <div className="svg-cont"><TriangleIcon /></div></button>
+              <ul className={toggleSubMenu ? 'active' : null}>
                 <li style={{transitionDelay: `${baseDelay * 2}s`}}><Link to="/film/shorts">SHORTS</Link></li>
                 <li style={{transitionDelay: `${baseDelay * 3}s`}}><Link to="/film/documentaries">DOCUMENTARIES</Link></li>
                 <li style={{transitionDelay: `${baseDelay * 4}s`}}><Link to="/film/personal">PERSONAL</Link></li>
