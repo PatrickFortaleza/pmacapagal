42d4f4efd7f6599349840173c1bca4a532d5a189
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-03 13:48:12 -0800

    #2 animate toggle menu

--- src/components/global/_Header/Navigation.js
+++ src/components/global/_Header/Navigation.js
@@ -4,24 +4,25 @@ import NavFooter from './NavFooter';
 import { Link } from 'react-router-dom';
 
 export default function Navigation(props) {
-  const toggleMenu = props.toggle
+  const toggleMenu = props.toggle;
+  const baseDelay = 0.075;
   return (
     <div className={ toggleMenu ? "Navigation active" : "Navigation"}>
       <Section>
         <nav className="main">
           <ul>
-            <li className="has-submenu">
+            <li className="has-submenu" style={{transitionDelay: `${baseDelay * 1}s`}}>
               <button>FILM</button>
               <ul>
-                <li><Link to="/film/shorts">SHORTS</Link></li>
-                <li><Link to="/film/documentaries">DOCUMENTARIES</Link></li>
-                <li><Link to="/film/personal">PERSONAL</Link></li>
+                <li style={{transitionDelay: `${baseDelay * 2}s`}}><Link to="/film/shorts">SHORTS</Link></li>
+                <li style={{transitionDelay: `${baseDelay * 3}s`}}><Link to="/film/documentaries">DOCUMENTARIES</Link></li>
+                <li style={{transitionDelay: `${baseDelay * 4}s`}}><Link to="/film/personal">PERSONAL</Link></li>
               </ul>
             </li>
-            <li><Link to="/photography">PHOTOGRAPHY</Link></li>
-            <li><Link to="/about">ABOUT</Link></li>
-            <li><Link to="/contact">CONTACT</Link></li>
-            <li><Link to="/resume">RESUME</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 5}s`}}><Link to="/photography">PHOTOGRAPHY</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 6}s`}}><Link to="/about">ABOUT</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 7}s`}}><Link to="/contact">CONTACT</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 8}s`}}><Link to="/resume">RESUME</Link></li>
           </ul>
         </nav>
       </Section>
