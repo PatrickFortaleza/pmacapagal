02837d6edeb508369fa75b5796c63209bf31df0d
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-03 12:33:00 -0800

    init mobile header styles

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import Section from '../entity/Section'
+import Hamburger from './_Header/Hamburger'
 
 export default function Header() {
   return (
     <header>
-      
+      <Section>
+        <div className="pseudoHeader">
+          <div className="logo">
+            <h1 className="logo">PATRICIA MACAPAGAL</h1>
+            <h2 className="logo sub">DIRECTOR OF PHOTOGRAPHY</h2>
+          </div>
+          <Hamburger />
+        </div>
+      </Section>
     </header>
   )
 }
---
+++ src/components/global/_Header/Hamburger.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hamburger() {
+  return (
+    <a id="icon" className='icon'>
+      <div className='hamburger'></div>
+    </a>
+  )
+}
