61ac1f3aa2ca1fffa27d8219dd218eb8270577bb
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-04 10:19:02 -0800

    #3 structure footer component

--- src/components/global/Footer.js
+++ src/components/global/Footer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Section from '../entity/Section'
+import SocialNav from './SocialNav'
 
 export default function Footer() {
+  const date = new Date();
+  const year = date.getFullYear()
   return (
-    <div>
-      Footer
-    </div>
+    <footer className="Footer">
+      <Section>
+        <div className="Footer__flex">
+          <div className="Footer__meta">
+            <p className="mime">PATRICIA MACAPAGAL &copy; {year}</p>
+          </div>
+        </div>
+        <SocialNav />
+      </Section>
+    </footer>
   )
 }
