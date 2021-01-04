e7d3bf1b27d1a21af906d0f66833c0db8f4c2831
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-04 11:19:08 -0800

    #7 structure film data v1

---
+++ src/data/films.js
@@ -0,0 +1,122 @@
+export const films = [
+  {
+    title: 'The Midnight Sweethearts',
+    category: 'short',
+    id: 1,
+    slug: 'the-midnight-sweethearts',
+    stills: [
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/themidnightsweethearts-01.png",
+        alt: "The Midnight Sweethearts still 01"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/themidnightsweethearts-02.png",
+        alt: "The Midnight Sweethearts still 02"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/themidnightsweethearts-03.png",
+        alt: "The Midnight Sweethearts still 03"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/themidnightsweethearts-04.png",
+        alt: "The Midnight Sweethearts still 04"
+      },
+    ]
+  },
+  {
+    title: 'Risky Biscuits',
+    category: 'short',
+    id: 2,
+    slug: 'risky-biscuits',
+    stills: [
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/riskybiscuits-01.png",
+        alt: "Risky Biscuits still 01"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/riskybiscuits-02.png",
+        alt: "Risky Biscuits still 02"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/riskybiscuits-03.png",
+        alt: "Risky Biscuits still 03"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/riskybiscuits-04.png",
+        alt: "Risky Biscuits still 04"
+      },
+    ]
+  },
+  {
+    title: 'Specula',
+    category: 'short',
+    id: 3,
+    slug: 'specula',
+    stills: [
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/specula-01.png",
+        alt: "Specula still 01"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/specula-02.png",
+        alt: "Specula still 02"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/specula-03.png",
+        alt: "Specula still 03"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/specula-04.png",
+        alt: "Specula still 04"
+      },
+    ]
+  },
+  {
+    title: 'Immanuel Part III',
+    category: 'short',
+    id: 4,
+    slug: 'immanuuel-part-3',
+    stills: [
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-01.png",
+        alt: "Specula still 01"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-02.png",
+        alt: "Specula still 02"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-03.png",
+        alt: "Specula still 03"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-04.png",
+        alt: "Specula still 04"
+      },
+    ]
+  },
+  {
+    title: 'Immanuel Part II',
+    category: 'short',
+    id: 5,
+    slug: 'immanuuel-part-2',
+    stills: [
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-04.png",
+        alt: "Specula still 01"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-03.png",
+        alt: "Specula still 02"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-01.png",
+        alt: "Specula still 03"
+      },
+      {
+        url: "https://pmacapagal-assets.s3-us-west-2.amazonaws.com/immanuelpart3-02.png",
+        alt: "Specula still 04"
+      },
+    ]
+  },
+]
