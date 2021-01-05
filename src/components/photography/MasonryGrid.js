15a18cf06ca1eb9587f95e9a2708f0940dc77f28
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-05 11:35:19 -0800

    #8 init loading screen for photography page

--- src/components/photography/MasonryGrid.js
+++ src/components/photography/MasonryGrid.js
@@ -1,32 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { photos } from '../../data/photography'
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry"
+import CubePreloader from '../entity/CubePreloader'
 
 export default function MasonryGrid({currentTab}) {
-  console.log(currentTab)
-  
+  const [loaded, upLoadCounter] = useState(() => { return false })
   let filteredPhotos;
-  if(currentTab === 'all'){
-    filteredPhotos = photos
-  }else{
-    filteredPhotos = photos.filter(p => { return p.category === currentTab })
+
+  const incrementLoadCounter = () => {
+    console.log('last image loaded')
+    setTimeout(() => {
+      upLoadCounter(true)
+    }, 200)
   }
 
-  useEffect(() => {
-    console.log(filteredPhotos)
+  const handleTabChange = () => {
     if(currentTab === 'all'){
       filteredPhotos = photos
+    }else{
+      filteredPhotos = photos.filter(p => { return p.category === currentTab })
     }
+  }
+
+  handleTabChange()
+
+  useEffect(() => {
+    handleTabChange()
   }, [currentTab])
 
   return (
     <div className="MasonryGrid">
+        {
+          loaded ? null : <CubePreloader />
+        }
         <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>
           <Masonry gutter={30}>
             {
               filteredPhotos.map((p, index) => {
                 return (
-                  <img key={index} src={p.url} />
+                  <img 
+                    key={index} 
+                    src={p.url}
+                    alt={p.alt}
+                    onLoad={index === filteredPhotos.length - 1 ? incrementLoadCounter : undefined }
+                  />
                 )
               })
             }
