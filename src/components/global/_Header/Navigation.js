4a191cae2c5b2341bb25044a73eac0385b175f0c
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-07 10:30:52 -0800

    add home link in desktop + tablet styles

--- src/components/global/_Header/Navigation.js
+++ src/components/global/_Header/Navigation.js
@@ -54,22 +54,23 @@ export default function Navigation(props) {
       <Section>
         <nav className="main">
           <ul>
+            <li className="home__link" style={{transitionDelay: `${baseDelay * 1}s`}}><Link to="/">HOME</Link></li>
             <li 
-              className={ toggleSubMenu ? "has-submenu active" : "has-submenu"} style={{transitionDelay: `${baseDelay * 1}s`}}
+              className={ toggleSubMenu ? "has-submenu active" : "has-submenu"} style={{transitionDelay: `${baseDelay * 2}s`}}
               onMouseEnter={setHoverOn}
               onMouseLeave={setHoverOff}
             >
               <button onClick={setClick}>FILM <div className="svg-cont triangle"><TriangleIcon /></div></button>
                 <ul className={toggleSubMenu ? 'active' : null}>
-                  <li style={{transitionDelay: `${baseDelay * 2}s`}}><Link to="/film/shorts">SHORTS</Link></li>
-                  <li style={{transitionDelay: `${baseDelay * 3}s`}}><Link to="/film/documentaries">DOCUMENTARIES</Link></li>
-                  <li style={{transitionDelay: `${baseDelay * 4}s`}}><Link to="/film/personal">PERSONAL</Link></li>
+                  <li style={{transitionDelay: `${baseDelay * 3}s`}}><Link to="/film/shorts">SHORTS</Link></li>
+                  <li style={{transitionDelay: `${baseDelay * 4}s`}}><Link to="/film/documentaries">DOCUMENTARIES</Link></li>
+                  <li style={{transitionDelay: `${baseDelay * 5}s`}}><Link to="/film/personal">PERSONAL</Link></li>
                 </ul>
             </li>
-            <li style={{transitionDelay: `${baseDelay * 5}s`}}><Link to="/photography">PHOTOGRAPHY</Link></li>
-            <li style={{transitionDelay: `${baseDelay * 6}s`}}><Link to="/about">ABOUT</Link></li>
-            <li style={{transitionDelay: `${baseDelay * 7}s`}}><Link to="/contact">CONTACT</Link></li>
-            <li style={{transitionDelay: `${baseDelay * 8}s`}}><Link to="/resume">RESUME</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 6}s`}}><Link to="/photography">PHOTOGRAPHY</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 7}s`}}><Link to="/about">ABOUT</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 8}s`}}><Link to="/contact">CONTACT</Link></li>
+            <li style={{transitionDelay: `${baseDelay * 9}s`}}><Link to="/resume">RESUME</Link></li>
           </ul>
         </nav>
       </Section>
