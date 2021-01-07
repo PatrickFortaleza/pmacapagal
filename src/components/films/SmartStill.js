cc5621f488ad126a5b0bd741507b3424a774a0fa
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-01-06 17:10:17 -0800

    #12 smart still component

--- src/components/films/SmartStill.js
+++ src/components/films/SmartStill.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Lightbox from '../photography/Lightbox'
 
 export default function SmartStill({ src, alt, hires }) {
   const [activeLightBox, toggleLightBox] = useState(() => { return false })
+  const [inView, toggleView] = useState(() => { return false })
+  const offset = 0
+  const img = useRef()
 
   const handleToggleLightBox = () => {
     toggleLightBox(true)
@@ -12,11 +15,43 @@ export default function SmartStill({ src, alt, hires }) {
     toggleLightBox(childData)
   }
 
+  const onScroll = () => {
+    if (!img.current) {
+      toggleView(false)
+      return
+    }
+    // Get bounding rectangle of the image
+    const rect = img.current.getBoundingClientRect()
+
+    // Checks if top and bottom of img has surpassed the top of viewport
+    const topInViewScrollUp = rect.top + offset >= 0;
+    const botInViewScrollUp = rect.bottom + offset >= 0
+    // Checks if top and bottom of img has surpassed the bottom of view port
+    const topInViewScrollDown = rect.top + offset <= window.innerHeight
+    const botInViewScrollDown = rect.bottom + offset <= window.innerHeight
+
+    if((!topInViewScrollUp && !botInViewScrollUp) || (!topInViewScrollDown && !botInViewScrollDown)){
+      // If the image is COMPLETELY out of the viewport, toggleView false
+      toggleView(false)
+    }else{
+      // If any part of the image is within the viewport, toggleView true
+      toggleView(true)
+    }
+  }
+
+  useEffect(() => {
+    onScroll()
+    document.addEventListener('scroll', onScroll)
+    return (() => document.removeEventListener('scroll', onScroll))
+  })
+
   return (
     <div className="SmartStill">
       { activeLightBox ? <Lightbox toggle={handleToggleOffLightBox} url={hires} alt={alt} /> : null }
       <img
-        onClick={handleToggleLightBox} 
+        onClick={handleToggleLightBox}
+        className={inView ? 'animate' : ''}
+        ref={img} 
         src={src}
         alt={alt}
       />      
