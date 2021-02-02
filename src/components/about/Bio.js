6d0969337dde9a4f8533363dfcf0edb253989b29
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-02-02 12:35:11 -0800

    update about bio

--- src/components/about/Bio.js
+++ src/components/about/Bio.js
@@ -14,11 +14,9 @@ export default function Bio() {
       <div className="Bio__container">
         <span className={loaded ? `animated` : undefined}>
           <img src={Headshot} alt="Patricia Headshot" className={loaded ? `animated` : undefined } onLoad={handleLoad}/>
-          <p>Patricia is currently in her second year of the Television and Video Production program at the British Columbia Institute of Technology. She completed a certificate from the Bachelors of Motion Picture Arts program at Capilano University. During her studies, she focused on developing her skill set in Cinematography and Sound Design. In her last semester, she is set to direct a short. </p>
+          <p>Patricia is currently in her second year of the Television and Video Production program at the British Columbia Institute of Technology. She completed a certificate from the Bachelors of Motion Picture Arts program at Capilano University. During her studies, she focused on developing her skill set in Cinematography and Sound Design. In her last semester, she is set to direct a short.  </p>
           <br/> <br/>
-          <p>When the opportunity arises, Patricia has been a part of small budget productions, as Director of Photography, First Camera Assistant and Stills Photographer.</p>
-          <br/> <br/>
-          <p>Patricia Macapagal is ready for new challenges and experiences.</p>
+          <p>When the opportunity arises, Patricia has been a part of independent productions in varying roles such as: Director of Photography, First Camera Assistant, and Stills Photographer.</p>
           <SocialNav />
         </span>
       </div>
